5068f9f413a2332c3113a4833c25238bf1a9cf03
Reynald516 <reynaldintelligence@gmail.com>
2025-06-24 13:23:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -129,5 +129,5 @@ def webhook():
         print("❌ ERROR di webhook:", e)
         return jsonify({"error": str(e)}), 500
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
